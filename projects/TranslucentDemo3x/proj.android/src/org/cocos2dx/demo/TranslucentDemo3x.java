c42e09fe226bedfd548dda96cd1854222cfec8ba
LinWenhai <wenhai.lin@chukong-inc.com>
2013-11-24 18:44:48 +0800

    add android ui sample

--- projects/TranslucentDemo3x/proj.android/src/org/cocos2dx/demo/TranslucentDemo3x.java
+++ projects/TranslucentDemo3x/proj.android/src/org/cocos2dx/demo/TranslucentDemo3x.java
@@ -13,4 +13,24 @@ public class TranslucentDemo3x extends NativeActivity{
 		
 		getWindow().setFormat(PixelFormat.RGBA_8888);
 	}
+	
+	@Override
+	public void onAttachedToWindow() {
+		RelativeLayout relativeLayout = new RelativeLayout(this);	
+		WindowManager.LayoutParams wParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, 
+				WindowManager.LayoutParams.WRAP_CONTENT,
+				WindowManager.LayoutParams.FIRST_APPLICATION_WINDOW, 40, PixelFormat.TRANSLUCENT);
+		
+		RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT
+				,RelativeLayout.LayoutParams.WRAP_CONTENT);			
+		TextView textView = new TextView(this);
+		textView.setTextSize(36);
+		textView.setTextColor(Color.RED);
+		textView.setText("Android Button");
+		relativeLayout.addView(textView,rParams);
+		
+		getWindowManager().addView(relativeLayout,wParams);
+		
+		super.onAttachedToWindow();
+	}
 }
