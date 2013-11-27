e4e054c8de924ff9433fdf8bda7e55d0169867bf
LinWenhai <wenhai.lin@chukong-inc.com>
2013-11-27 11:14:45 +0800

    fix TRANSLUCENT effect not works as expected.

--- cocos/2d/platform/android/nativeactivity.cpp
+++ cocos/2d/platform/android/nativeactivity.cpp
@@ -155,9 +155,7 @@ static cocos_dimensions engine_init_display(struct engine* engine) {
     const EGLint attribs[] = {
             EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
             EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-            EGL_BLUE_SIZE, 5,
-            EGL_GREEN_SIZE, 6,
-            EGL_RED_SIZE, 5,
+            EGL_BUFFER_SIZE,32,
             EGL_DEPTH_SIZE, 16,
             EGL_STENCIL_SIZE, 8,
             EGL_NONE
